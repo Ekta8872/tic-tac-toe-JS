5957e38375d35bd69f7407a4c906280415748bf9
Ekta kumari <ektakumari8872@gamil.com>
2024-10-23 17:08:39 +0530

    Related Task (s):- code complete

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelector("#reset");
+let resetBtn = document.querySelector("#resetBtn");
 let newBtn = document.querySelector("#new-btn");
 let msgContainer = document.querySelector(".msgContainer");
 let msg = document.querySelector(".winnerPara");
 
 let turn0 = true;
+let count = 0;
 let winPatterns = [
   [0, 1, 2],
   [0, 3, 6],
@@ -15,6 +16,12 @@ let winPatterns = [
   [3, 4, 5],
   [6, 7, 8],
 ];
+const resetGame = () => {
+  turn0 = true;
+  count = 0;
+  enabledBox();
+  msgContainer.classList.add("hide");
+};
 boxes.forEach((box) => {
   box.addEventListener("click", () => {
     console.log("Clicked");
@@ -26,13 +33,20 @@ boxes.forEach((box) => {
       turn0 = true;
     }
     box.disabled = true;
-    checkWinner();
+    count++;
+    let isWinner = checkWinner();
+
+    if (count === 9 && !isWinner) {
+      gameDraw();
+    }
   });
 });
-const resetGame = () => {
-  turn0 = true;
-  enabledBox();
+const gameDraw = () => {
+  msg.innerText = `Game was a Draw.`;
+  msgContainer.classList.remove("hide");
+  disabledBox();
 };
+
 const disabledBox = () => {
   for (let box of boxes) {
     box.disabled = true;
@@ -41,6 +55,7 @@ const disabledBox = () => {
 const enabledBox = () => {
   for (let box of boxes) {
     box.disabled = false;
+    box.innerText = "";
   }
 };
 const showWinner = (winner) => {
@@ -57,9 +72,14 @@ const checkWinner = () => {
     let posVal3 = boxes[pattern[2]].innerText;
     if (posVal1 !== "" && posVal2 !== "" && posVal3 !== "") {
       if (posVal1 === posVal2 && posVal2 === posVal3) {
-        console.log("winner", posVal1);
         showWinner(posVal1);
       }
     }
   }
 };
+
+resetBtn.addEventListener("click", () => {
+  console.log("clicked");
+  resetGame();
+});
+newBtn.addEventListener("click", resetGame);
